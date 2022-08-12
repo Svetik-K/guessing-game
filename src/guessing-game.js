417e3a0b4348fcd6ae3b085d423ea86bf671f34b
Svetik-K <sviatlana_kliautsova@icloud.com>
2022-08-12 11:30:34 +0200

    feat: guessing game code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
+        return this;
     }
 
     guess() {
-
+        this.result = this.minValue + Math.ceil((this.maxValue - this.minValue) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.maxValue = this.result;
     }
 
     greater() {
-
+        this.minValue = this.result;
     }
 }
+
 
 module.exports = GuessingGame;
